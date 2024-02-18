8f90b544e890bc833c6b014c960d062e720a6d8f
Brian Donaldson <nairbsd@gmail.com>
2024-02-17 19:23:44 -0600

    example jsdoc generator extension

--- packages/banana-web-app/src/features/banana-spinner/components/BananaSpinner.tsx
+++ packages/banana-web-app/src/features/banana-spinner/components/BananaSpinner.tsx
@@ -1,3 +1,10 @@
+/**
+ * Description placeholder
+ * @date 2/17/2024 - 7:22:13 PM
+ *
+ * @export
+ * @returns {*}
+ */
 export default function BananaSpinner() {
   return <div>BananaSpinner</div>;
 }
